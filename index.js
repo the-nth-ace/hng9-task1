f59d75ced46d456e384e8c0ff83d10c13f1678f8
Femi Olatubosun <thefemiolatubosun@gmail.com>
2022-10-28 12:25:53 +0100

    Last commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors({
+    origin: '*'
+}))
+    .get('/', (req, res) => {
+
+        res.send({
+            slackUsername: 'thefemiolatubosun', backend: true, age: 24, bio: "I'm Femi. And I am excited to be a part of the internship!"
+        })
+    }).listen(port, () => {
+        console.clear()
+        console.log(`Epxress server listening on port ${port}`)
+    })
+
